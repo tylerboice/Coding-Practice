f462d3b4ecc4e4523ff5d680f80c300dc6b41ecd
Tyler Boice <tylerboice@users.noreply.github.com>
2017-08-15 01:50:44 -0700

    Add files via upload

--- Color Maze/src/ColorMaze.java
+++ Color Maze/src/ColorMaze.java
@@ -11,24 +11,26 @@ public class ColorMaze {
     private int size;
     private String path = "";
 
+    // Initializes the ColorMaze object
     public ColorMaze() {
         legend = null;
         maze = new char[1000][1000];
         size = 0;
     }
 
+    // Generates a maze and legend based off a text file
     public void initializeMaze(String filename) {
-        // Initialize variables for reading file and tracking the lines
         try {
+            // Initialize variables for reading file and tracking the current line
             String currentLine = null;
             int mazeLine = 0;
             FileReader fr = new FileReader(filename);
             BufferedReader br = new BufferedReader(fr);
 
-            // setup the legend for the maze which is always the first line of text
+            // Set up the legend for the maze which is always the first line of text
             setLegend(br.readLine().replace(" ", ""));
 
-            // While the maze still has lines, put the lines into the maze variable
+            // While the maze still has lines, translate the lines into the maze array
             while ((currentLine = br.readLine()) != null) {
                 setMaze(mazeLine++, (currentLine.replace(" ", "")));
                 size = mazeLine;
@@ -45,13 +47,14 @@ public class ColorMaze {
         }
     }
 
-    // set the legend for the maze
+    // Sets the legend for the maze
     public void setLegend(String legendString) {
         this.legend = legendString.toCharArray();
     }
 
     // Returns the legend in String format
     public String legendString() {
+        // Create a temp string to hold the information that needs to be printed
         String tempString = "";
         for (int i = 0; i < legend.length; i++) {
             tempString += legend[i];
@@ -59,7 +62,7 @@ public class ColorMaze {
         return tempString;
     }
 
-    //set the values for the maze
+    // Set the values for the maze
     public void setMaze(int row, String mazeString) {
         for (int i = 0; i < mazeString.length(); i++) {
             this.maze[row][i] = mazeString.charAt(i);
@@ -68,7 +71,9 @@ public class ColorMaze {
 
     // Returns the string for the maze
     public String mazeString() {
+        // Creates a temp string to hold the information that needs to be printed
         String tempString = "";
+        // Loop though every items in the maze array and put its value into tempString
         for (int row = 0; row < size*size; row++) {
             for (int col = 0; col < size; col++) {
                 tempString += maze[row][col];
@@ -78,25 +83,30 @@ public class ColorMaze {
         return tempString.trim();
     }
 
-    //Finds the first viable path through the maze
-    public void findPath() {
+    // Returns the string for the path
+    public String pathString(){
+        findPathEntrance();
+        return path.trim();
+    }
+
+    // Looks for the beginning of the path
+    public void findPathEntrance() {
+        // All paths must start in the bottom row of the maze
         for ( int col = 0; col < size-1; col++){
+            // If the value at a given point is in the legend, then begin finding a path and generating the path string
             if(isAllowed(maze[size-1][col])){
-                path += "(" + col + "," + (size-1) + ")\n";
-                if (pathRecursion(size-1, col)) {
+                path += "(" + col + ", " + (size-1) + ")\n";
+                // The beginning of the path is always assumed to start upwards since downards is not an option
+                if (pathRecursion(size-1, col, 'u')) {
                     return;
                 }
             }
         }
     }
 
-    public String printPath(){
-        findPath();
-        return path.trim();
-    }
-
-    //determines if a letter is allowed by the legend
+    // Determines if a letter is allowed by the legend
     public boolean isAllowed(char letter){
+        // For each letter in the legend return true if the letter is in the legend and false if it is not
         for( char item : legend) {
             if( item == letter) {
                 return true;
@@ -106,30 +116,35 @@ public class ColorMaze {
     }
 
     // Looks for paths to follow by checking, up, right, down, and left
-    public boolean pathRecursion(int row, int col) {
-        // The condition to complete the maze is to reach the last row
+    public boolean pathRecursion(int row, int col, char direction) {
+        // This function will prioritize going up and to the right since the path attempts to start form the bottom left
+        // If the path reaches the top row, then the path is complete
         if (row == 0) {
             return true;
         }
-        // go up if possible
-        if (isAllowed(maze[row-1][col])) {
-            path += "(" + col + "," + (row-1) + ")\n";
-            pathRecursion(row-1, col);
+        try {
+            // The path will go up, unless the path is headed down
+            if (isAllowed(maze[row - 1][col]) && direction != 'd') {
+                path += "(" + col + ", " + (row - 1) + ")\n";
+                pathRecursion(row - 1, col, 'u');
+            }
+            // The path will go right, unless the path is headed left
+            else if (isAllowed(maze[row][col + 1]) && direction != 'l') {
+                path += "(" + (col + 1) + ", " + row + ")\n";
+                pathRecursion(row, col + 1, 'r');
+            }
+            // The path will go left, unless the path is headed right
+            else if (isAllowed(maze[row][col - 1]) && direction != 'r') {
+                path += "(" + (col - 1) + ", " + row + ")\n";
+                pathRecursion(row, col - 1, 'l');
+            }
+            // The path will go down, unless the path is headed up
+            else if (isAllowed(maze[row + 1][col]) && direction != 'u') {
+                path += "(" + col + ", " + (row + 1) + ")\n";
+                pathRecursion(row + 1, col, 'd');
+            }
         }
-        // go right if possible
-        else if(isAllowed(maze[row][col+1])) {
-            path += "(" + (col + 1) + "," + row + ")\n";
-            pathRecursion(row, col + 1);
-        }
-        // go down if possible
-        else if(isAllowed(maze[row+1][col])) {
-            path += "(" + col + "," + (row + 1) + ")\n";
-            pathRecursion(row+1, col);
-        }
-        // go left if possible
-        else if(isAllowed(maze[row][col-1])) {
-            path += "(" + (col-1) + "," + row + ")\n";
-            pathRecursion(row, col-1);
+        catch (IndexOutOfBoundsException e){
         }
         return false;
     }
--- Color Maze/test/ColorMazeTester.java
+++ Color Maze/test/ColorMazeTester.java
@@ -23,8 +23,42 @@ public class ColorMazeTester {
     @Test
     public void testCorrectPath() {
         ColorMaze tester = new ColorMaze();
-        String testPath = "(1,4)\n(1,3)\n(1,2)\n(2,2)\n(3,2)\n(3,1)\n(3,0)";
+        String testPath = "(1, 4)\n(1, 3)\n(1, 2)\n(2, 2)\n(3, 2)\n(3, 1)\n(3, 0)";
         tester.initializeMaze("InputMaze.txt");
-        Assert.assertEquals(testPath, tester.printPath());
+        Assert.assertEquals(testPath, tester.pathString());
     }
+
+    @Test
+    public void testPath() {
+        ColorMaze tester = new ColorMaze();
+        tester.initializeMaze("InputMaze.txt");
+        tester.pathString();
+    }
+
+    @Test
+    public void testChallengeLegend() {
+        ColorMaze tester = new ColorMaze();
+        String testLegend = "ROYPO";
+        tester.initializeMaze("InputChallenge.txt");
+        Assert.assertEquals(testLegend, tester.legendString());
+    }
+
+    @Test
+    public void testChallengeMaze() {
+        ColorMaze tester = new ColorMaze();
+        String testMaze = "RRBRRRBPYGPBBBGPBPPR\nBGYPRPYYORYPPYYRRRPP\nBPGROPYGRYYGPORYPBOO\nRBBORPYOOYRPBRGRBGPG\n" +
+                "RPYGGGPYPYOGBORYPBYO\nORBGBYBPGRPYROGYGYRP\nBGOOOGBBROYYYYPBYYGG\nPPGBOPYGBROGBGROYRBR\n" +
+                "YYPPRBYBPOOGPYRPPYRY\nPOOBBBGOYGOPBGYRRYRB\nPPYRBOORORYBGBGOOPBY\nBBRGYGPYGPRRPYGOOYRR\n" +
+                "OGRYBPYOPBRYBGPGOOBP\nRYGPGGORYOOGRGPPYPBG\nPYPROOROYRPORYPYBBYR\nOYPGRPRGPOBBRBOBYYBP\n" +
+                "BYYPOYOYORBRGGYGRGYG\nYBYYGBRROBOPPOBORRRP\nPOOOPYGGYPOGPOBGPRPB\nRBBRRRRBBBYOBGPGGOOY";
+        tester.initializeMaze("InputChallenge.txt");
+        Assert.assertEquals(testMaze, tester.mazeString());
+    }
+
+   @Test
+   public void testChallengePath() {
+       ColorMaze tester = new ColorMaze();
+       tester.initializeMaze("InputChallenge.txt");
+       System.out.println(tester.pathString());
+   }
 }
